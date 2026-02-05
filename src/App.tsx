7648cbd54eba4b3c26f80955b1fa8fe3edb884a9
unknown <0228903@up.edu.mx>
2026-02-05 11:13:13 -0600

    Added example of use of redux query.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import "./App.css";
 import viteLogo from "/vite.svg";
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { useAppDispatch, useAppSelector } from "./app/hooks";
 import { incremented, amountAdded } from "./features/counter/counterSlice";
+import { useFetchBreedsQuery } from "./features/redux-query/redux-query-slice";
 
 function App() {
   const dispatch = useAppDispatch();
   const count = useAppSelector((state) => state.counter.value);
+
+  const [numDogs, setNumDogs] = useState<number>(10);
+
+  const { data = [], isFetching } = useFetchBreedsQuery(numDogs);
 
   const handleAmountAdd = () => {
     dispatch(amountAdded(5));
   };
 
   const handleSingleAdd = () => {
-    dispatch(incremented())
-  }
+    dispatch(incremented());
+  };
 
   return (
     <>
@@ -30,6 +36,42 @@ function App() {
       <div className="card">
         <button onClick={handleAmountAdd}>count is {count}</button>
         <button onClick={handleSingleAdd}>count is {count}</button>
+
+        <div>
+          <p>Dogs to fetch:</p>
+          <select
+            value={numDogs}
+            onChange={(e) => setNumDogs(Number(e.target.value))}
+          >
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+          </select>
+        </div>
+
+        <div>
+          <p>Number of dogs fetched: {data.length}</p>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Picture</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((breed) => (
+                <tr key={breed.id}>
+                  <td>{breed.name}</td>
+                  <td>
+                    <img src={breed.image.url} alt={breed.name} height={250} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
